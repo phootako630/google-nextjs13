4c27a6285a094ba646f51c6b701d33b731ed682d
ivanzhang <ivanzhang220820@gmail>
2023-03-10 16:38:27 -0500

    Initialize templates

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+        <>
+            <title>Google Clone Next.js 13 beta</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <meta name="description" content="Google Clone Next.js 13 beta" />
+            <link rel="icon" href="favicon.ico" />
+        </>
+    )
+}
