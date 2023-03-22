5689bdb7a9e70457ce4417c57005980fdb6dab55
ivanzhang <ivanzhang220820@gmail.com>
2023-03-21 22:28:42 -0400

    Added an image result page

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,6 +1,34 @@
-export default function ImageSearchPage() {
-    return (
-        <div>ImageSearchPage </div>
-    )
+import React from 'react';
+import Link from "next/link";
+import ImageSearchResults from "@/components/ImageSearchResults";
 
-}
+const ImageSearchPage = async ({ searchParams }) => {
+   // await new Promise((resolve) => setTimeout(resolve, 3000));
+    const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.
+        env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`);
+    if (!response.ok) {
+        throw new Error('Error fetching data')
+    }
+    const data = await response.json();
+    console.log(data)
+    const results = data.items;
+    if (!results) {
+        return (
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="text-3xl mb-4">No results found</h1>
+                <p className="text-lg">
+                    Try to search with more specific keywords or go back to home page{" "}
+                    <Link className="text-blue-600" href="/">Go back to home</Link>
+                </p>
+            </div>
+        )
+    }
+    return (
+        <>
+            {results && <ImageSearchResults results={data}/>}
+        </>
+
+    )
+};
+
+export default ImageSearchPage;
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import WebSearchResults from "@/components/WebSearchResults";
 
 export default async function WebSearchPage({searchParams}) {
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.
         env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
     if (!response.ok) {
