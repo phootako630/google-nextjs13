60a0895a5b10ed41cd912406ebf8ad33860e9b80
ivanzhang <ivanzhang220820@gmail.com>
2023-03-22 22:57:15 -0400

    Dynamic export

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 import React, {useCallback, useEffect, useRef, useState} from 'react';
 import Link from "next/link";
 import ImageSearchResults from "@/components/ImageSearchResults";
