efed6789cb241bf5ea02143d522905e44b1af448
ivanzhang <ivanzhang220820@gmail.com>
2023-03-22 20:16:45 -0400

    Add lazy-loading to ImageSearchResults.jsx and image/page.jsx

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -4,7 +4,7 @@ import WebSearchResults from "@/components/WebSearchResults";
 export default async function WebSearchPage({searchParams}) {
     await new Promise((resolve) => setTimeout(resolve, 3000));
     const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.
-        env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
+        env.NEXT_PUBLIC_GOOGLE_API_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${searchParams.searchTerm}`);
     if (!response.ok) {
         throw new Error('Error fetching data')
     }
